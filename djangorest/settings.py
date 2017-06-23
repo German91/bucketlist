d9a4a1bc7d007a4fc836fd4f59373c8a1553359a
German Hernandez <german@gearedapp.co.uk>
2017-06-23 16:21:13 +0100

    add missing dependencies

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -1,8 +1,8 @@
-import os
+import os, dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
