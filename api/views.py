e448afc156742fac381a5ca25f3aa689b4f8ce53
German Hernandez <german@gearedapp.co.uk>
2017-06-23 16:07:22 +0100

    first commit

---
+++ api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
+# Create your views here.
+class CreateView(generics.ListCreateAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
